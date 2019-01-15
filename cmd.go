6df0100f30236630e35c366a8e873237ff080a15
Nelo Mitranim <me@mitranim.com>
2019-01-15 11:51:24 +0300

    CSS-independent style of external link icon
    Fixes the icon style in RSS readers that don't fetch CSS or don't apply
    it properly.

--- cmd.go
+++ cmd.go
@@ -530,7 +530,7 @@ func writePublic(path string, bytes []byte) error {
 }
 
 var featherIconExternalLink = strings.TrimSpace(`
-<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="external-link-icon">
+<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" style="display: inline-block; width: 0.8em; height: 0.8em; margin-left: 0.3ch;">
 	<path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6" />
 	<polyline points="15 3 21 3 21 9" />
 	<line x1="10" y1="14" x2="21" y2="3" />
